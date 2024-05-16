ab4fb6a4de1ee8461d5ec94a45935a8b32861214
samian2002 <samiantariq14@gmail.com>
2024-05-16 15:32:24 +0200

    bonus

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+/*
 const { createApp } = Vue;
 
 createApp({
@@ -18,6 +19,39 @@ createApp({
         removeTodo(index) {
             this.todos.splice(index, 1); // Rimuove il todo in base all'indice
             console.log("ToDo rimosso:", this.todos); // Stampa l'array aggiornato in console
+        }
+    }
+}).mount('#app');
+*/
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            todos: [
+                { title: 'Fare la spesa', done: false },
+                { title: 'Leggere un libro', done: true },
+                { title: 'Allenarsi', done: false }
+            ],
+            newTodo: ''
+        };
+    },
+    methods: {
+        addTodo() {
+            if (this.newTodo !== '') {
+                console.log("Nuovo ToDo aggiunto:", this.newTodo); 
+                this.todos.push({ title: this.newTodo, done: false }); // Aggiunge il nuovo todo come oggetto
+                this.newTodo = '';
+            }
+        },
+        removeTodo(index) {
+            this.todos.splice(index, 1); // Rimuove il todo in base all'indice
+            console.log("ToDo rimosso:", this.todos); // Stampa l'array aggiornato in console
+        },
+        toggleDone(index) {
+            this.todos[index].done = !this.todos[index].done; // Inverte il booleano "done"
+            console.log("Stato ToDo modificato:", this.todos[index]); // Stampa lo stato modificato del ToDo
         }
     }
 }).mount('#app');
